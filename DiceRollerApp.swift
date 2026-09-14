7087afcc6d9c5c4375bb30abddaf0154449f358b
Aoi <aoimmitsu@gmail.com>
2026-09-14 19:55:16 +0300

    Assets branch 1

---
+++ DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Student2 on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
